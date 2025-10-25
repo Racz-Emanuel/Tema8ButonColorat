c556b5d4cac7a13e09f85f921f93e14b7c6b8ef0
Emi <emiracz05@email.com>
2025-10-25 17:07:44 +0300

    Am facut patratul

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,31 @@
-<script setup></script>
+<script setup>
+import{ onMounted, onUpdated } from 'vue';
+import { ref } from 'vue';
 
+onMounted(() => {
+  console.log('App mounted');
+});
+onUpdated(() => {
+  console.log('App updated');
+});
+
+const squareColor = ref('red');
+
+const changeColor =() =>{
+squareColor.value = '#' + Math.floor(Math.random() * 16777215).toString(16);
+}
+</script>
 <template>
-  <h1>You did it!</h1>
-  <p>
-    Visit <a href="https://vuejs.org/" target="_blank" rel="noopener">vuejs.org</a> to read the
-    documentation
-  </p>
+<button @click="changeColor">Genereaza Culoare</button>
+<div class="square" :style="{ backgroundColor: squareColor }"></div>
 </template>
 
-<style scoped></style>
+<style scoped>
+.square {
+  margin-top: 20px;
+  width: 100px;
+  height: 100px;
+  background-color: red;
+  margin-left: 15px;
+}
+</style>
